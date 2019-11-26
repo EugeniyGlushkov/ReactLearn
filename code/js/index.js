efdf76a0b09faebe5325fb97632518f1a0de10c5
ElenaGlushkova <amber_1@list.ru>
2019-11-26 08:55:51 +0300

    add nastr

---
+++ code/js/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <h1>fdf1111111111111111111fsfsfsfshgfhghfghff</h1>,
+  document.getElementById('fieldToShow')
+);
