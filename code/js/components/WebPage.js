7d6441eb83b57b80f34c00d238c5dac8f4332ed9
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2019-11-27 23:14:14 +0300

    add provider

---
+++ code/js/components/WebPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import '../../css/style';    подключение стилей
+
+const WebPage = () => (
+    <div>
+        <h2>Cars:</h2>
+        <hr/>
+        <h3>Details:</h3>
+    </div>
+);
+
+export default WebPage;
